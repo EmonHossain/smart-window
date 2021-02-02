23e34e99be702489d10001869204670aa53fafdc
mids <md-atikul.islam@s2019.tu-chemnitz.de>
2021-02-02 22:08:34 +0100

    Can detect object using yolov3. Resolves #1

--- Smart Window/app.py
+++ Smart Window/app.py
@@ -1,61 +1,77 @@
+import io
 import cv2 as cv2
 import requests
+import base64
+import numpy as np
 import json
 from flask import Flask, request, render_template
 from flask import Response
-
+from PIL import Image # pip  install pillow
+from imageai.Detection import ObjectDetection
+import os
 
 app = Flask(__name__)
-camera = cv2.VideoCapture(0)  # use 0 for web camera
-
-
-
-# for local webcam use cv2.VideoCapture(0)
-
-def gen_frames():  # generate frame by frame from camera
-    global geolocation, address
-    while True:
-        # Capture frame-by-frame
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-
-            frame = cv2.flip(frame, 1)
-            ret, buffer = cv2.imencode('.jpg', frame)
-            # video frame
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-
-
-@app.route('/video_feed')
-def video_feed():
-    # Video streaming route. Put this in the src attribute of an img tag
-
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+""" global varible variable """
+image_np = np.array([1, 2, 3, 4, 5])
 
 
 @app.route('/')
 def webcam():
     """Video streaming home page."""
-    global landmarks, museums, museums_distance, landmarks_distance, current_loc
     return render_template('index.html')
 
-@app.route('/getdata', methods=['POST'])
-def getdata():
-    """Getting the data form the ajax request and api"""
+@app.route('/URL_tesing')
+def URL_tesing():
+    """Video streaming home page."""
+    global image_np
+    print(image_np)
+    print(type(image_np))
+    return render_template('image_data.html')
+
+@app.route('/get_image_data', methods=['POST'])
+def get_image_data():
+    """Getting the image data in json form, from the ajax request"""
+    jsonResponse =  request.get_json()
+    json_data= {
+        'image': jsonResponse
+    }
+    image = jsonResponse['image']
+    base64_decoded = base64.b64decode(image.split(',')[1])
+    f = open("image.jpg", "wb")
+    f.write(base64_decoded)
+    f.close()
+    #global arr
+    #arr = cv2.bitwise_not(cv2.imread("temp.png", 0))
+    #image = Image.open(io.BytesIO(base64_decoded))
+    #global image_np
+    #image_np = np.array(image)
+    execution_path = os.getcwd()
+
+    detector = ObjectDetection()
+    detector.setModelTypeAsYOLOv3()
+    detector.setModelPath(os.path.join(execution_path, "yolo.h5"))
+    detector.loadModel()
+    detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path, "image.jpg"),
+                                                 output_image_path=os.path.join(execution_path, "imagenew.jpg"))
+
+    for eachObject in detections:
+        print(eachObject["name"], " : ", eachObject["percentage_probability"])
+    return Response(json.dumps(json_data))
+
+
+
+@app.route('/get_geolocation_data', methods=['POST'])
+def get_geolocation_data():
+    """Getting the data from the ajax request and api"""
     geolocation = request.get_json()
     loc = (str(geolocation['loc'])).split(", ")
     lat = float(loc[0])
     long = float(loc[1])
     # api-endpoint
     URL = "https://places.ls.hereapi.com/places/v1/discover/explore"
-
     # API key
     api_key = 'tCLOWQv2DtK02jWzK7lxRq933DNH8HrE7chf60cE_-c'
     # Defining a params dictionary for the parameters to be sent to the API
-
     PARAMS = {
         'at': '{},{}'.format(lat, long),
         'apikey': api_key,
