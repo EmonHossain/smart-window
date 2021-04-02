902af6df921eb787848a23178ed85fdd3f282c27
mids <md-atikul.islam@s2019.tu-chemnitz.de>
2021-04-02 13:03:56 +0200

    Endpoint for geolocation request added

--- Smart Window/stream/views.py
+++ Smart Window/stream/views.py
@@ -40,3 +40,14 @@ def image_analysis(request):
         # r_image, ObjectsList = image_detector.detect_img(img)
 
     return JsonResponse(json_data)
+    
+def current_geo_info(request):
+    if request.method == "POST":
+        geolocation = json.loads(request.body)
+        loc = (str(geolocation.get('loc'))).split(", ")
+        # lat = float(loc[0])
+        # long = float(loc[1])
+        print(geolocation)
+        json_data = GeoLocationInfo().get_api_data(loc[0], loc[1])
+        print(json_data)
+    return JsonResponse(json_data)    
