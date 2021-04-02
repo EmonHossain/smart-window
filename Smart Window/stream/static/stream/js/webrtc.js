07a3fb599601708a4b8452297e1767c26018d36c
Emon <emondipu@gmail.com>
2021-04-02 12:39:46 +0200

    Request for image analysis

--- Smart Window/stream/static/stream/js/webrtc.js
+++ Smart Window/stream/static/stream/js/webrtc.js
@@ -44,5 +44,80 @@ $(document).ready(function () {
         context.fillStyle = "#AAA";
         context.fillRect(0, 0, canvas.width, canvas.height);
     }
+	
+	//The clock will work only if video stream starts
+    function clockrun() {
+        if (streaming) {
+            (function tick() {
+                takephoto();
+                console.log("tic - toc");
+                window.setTimeout(tick, 1000);
+            }());
+        }
+    }
+
+    function sendToAnalysis(img64) {
+
+        var impos = new Object();
+        impos.img = img64.split(",")[1];
+
+        var token = $('input[name="csrfmiddlewaretoken"]').attr('value');
+
+
+        $.ajax({
+            url: '/img/analysis/',
+            type: 'POST',
+            dataType: 'json',
+            contentType:"application/json",
+            data: JSON.stringify(impos),
+            headers: {'contentType': 'application/json', 'X-CSRFToken': token}
+        })
+            .done(function (data) {
+                if(data.found && prevPoiTitle != data.poi) {
+
+                    $('#main-panel-poi-img').attr('src', data.img);
+                    $('#main-panel-poi-title').text(data.poi);
+                    $('#main-panel-poi-desc').text(data.desc);
+
+                    prevPoiTitle = data.poi;
+
+                    console.log("poi changed to "+data.poi);
+                    $('#poi-panel').removeAttr("hidden");
+                    $('#welcome-panel').hide();
+
+                    $('#img-q').prepend("<img src='"+data.img+"' >");
+
+                    if(data.poi =="Opera House Chemnitz"){
+                        $("#cn-opera").parent().remove();
+                    }else if (data.poi == "Karl marx monument"){
+                        $("#cn-karl").parent().remove();
+                    }
+                }
+            })
+            .fail(function () {
+                console.log("error");
+            })
+            .always(function () {
+                console.log("complete");
+            });
+    }
+    var pressCounter = 0;
+    $(this).keypress(function(e) {
+
+        if(e.which == 97 ){
+            if (pressCounter <= locations.length) {
+                $("#loc-now").text(locations[pressCounter]);
+                pressCounter++;
+            }else {
+                pressCounter = 0;
+            }
+        }
+        else if(e.which == 99){
+            var nth = $(".info-next").children();
+            if(nth.length > 1){
+                nth.first().remove();
+            }
+        }
+    });
 
 });
